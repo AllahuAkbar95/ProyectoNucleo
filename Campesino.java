57ae6010c5fdbff62ab7af6221c77d4cac7b0ab0
Anggelo Novack <behemothsl@gmail.com>
2016-03-24 23:29:41 -0500

    Update: cambios a los tipos de datos

--- Campesino.java
+++ Campesino.java
@@ -1,16 +1,24 @@
 package ProyectTests.Tests;
 
-public class Campesino extends Persona {
+import java.util.List;
 
-	public Campesino(String nNombre, int nTel1, int nTel2, int nDocumento, String nUsuario, String nContrasenia,
-			String nCorreo) {
-		super(nNombre, nTel1, nTel2, nDocumento, nUsuario, nContrasenia, nCorreo);
+public class Campesino extends Persona {
+	private List<Producto> notificaciones;
+
+	public Campesino(String nNombre, String nTel1, String nTel2, String nDocumento, String nUsuario, String nContrasenia,
+			String nCorreo, String nDireccion) {
+		super(nNombre, nTel1, nTel2, nDocumento, nUsuario, nContrasenia, nCorreo, nDireccion);
 		// TODO Auto-generated constructor stub
+		this.notificaciones = null;
 	}
 
-	
-	
-	
+	public List<Producto> getNotificaciones() {
+		return notificaciones;
+	}
+
+	public void setNotificaciones(List<Producto> notificaciones) {
+		this.notificaciones = notificaciones;
+	}
+
 
 }
-
