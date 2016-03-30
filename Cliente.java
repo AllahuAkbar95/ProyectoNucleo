123d084e69865369dec32dfae561e222bc914170
Anggelo Novack <behemothsl@gmail.com>
2016-03-29 23:21:12 -0500

    Agrego clase cliente

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+package ProyectTests.Tests;
+
+import java.util.List;
+
+public class Cliente extends Persona{
+
+	public Cliente(String nDocumento,String nNombre, String nTel1, String nTel2, String nUsuario, String nContrasenia,
+			String nCorreo, String nDireccion, List<Producto> nLista) {
+		super(nDocumento,nNombre, nTel1, nTel2,  nUsuario, nContrasenia, nCorreo, nDireccion, nLista);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
