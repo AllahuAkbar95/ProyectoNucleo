0014a5566b0c637ec1a9cb4191b2b07504fddc4b
Anggelo Novack <behemothsl@gmail.com>
2016-03-29 23:22:04 -0500

    Update: cambios a los tipos de datos y agrego atributos

--- Producto.java
+++ Producto.java
@@ -3,15 +3,50 @@ package ProyectTests.Tests;
 public class Producto {
 
 	private String nombre;
-	private double precio;
-	private String cantidad; // por ahora
-	private Campesino publicador;
+	private int precio;
+	private int cantidad; // por ahora
+	private String publicador;
 	
-	public Producto(String nNombre, double nPrecio, String nCantidad, Campesino nPublicador)
+	public Producto(String nNombre, int nPrecio, int nCantidad, String nPublicador)
 	{
 		this.nombre = nNombre;
 		this.precio = nPrecio;
 		this.cantidad = nCantidad;
 		this.publicador = nPublicador;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getPublicador() {
+		return publicador;
+	}
+
+	public void setPublicador(String publicador) {
+		this.publicador = publicador;
+	}
+	
+	
 }
+
