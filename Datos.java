7b66d864babb8488827202c3114f5b10b8e3af33
Anggelo Novack <behemothsl@gmail.com>
2016-04-17 18:23:57 -0500

    Clase Datos, contiene métodos para las validaciones

--- Datos.java
+++ Datos.java
@@ -1,61 +1,41 @@
-package Nucleo;
+package ProyectTests.Tests;
 
 public class Datos {
 
-	// Validacion de tamaño - MADE BY JM5 © ================
-	public static boolean validarTamaño(int min, String f, int max){
-		if(f.length()<=max && f.length()>=min){
-			if(this.validarCaracteres(f)==true){
-				return true;
-			}else{
-				return false;
-			}
+	public static boolean validarTamanio(int min, String f, int max){
+		if(f.length()<=max && f.length()>=min)
+		{
+			return true;
 		}
 		return false;
 	}
 
 	
-	// Validacion de caracteres - MADE BY JM5 © ================
-	public int validarLetras(String d){
-		short c=0;
-		if ((d).matches("([a-z]|[A-Z]|\\s)+")){ 
-			c=1;
-		} else{
-			c=0;
+	public static boolean validarLetras(String d){	
+		if ((d).matches("([a-z]|[A-Z]|\\s)+"))
+		{ 
+			return true;
+		} else
+		{
+			return false;
 		}
-		return c;
 	}
 
-	public int validarNumeros(String d){
-		short c=0;
-		if((d).matches("([0-9]|\\-)+")){
-			c=0;
-		}else{
-			c=1;
+	public static boolean validarNumeros(String d){
+		if((d).matches("([0-9]|\\-)+"))
+		{
+			return true;
+		}else
+		{
+			return false;
 		}
-		return c;
-	}
-
-	public boolean validarCaracteres(String d){
-		boolean f=false;
-		if(this.validarLetras(d)==1 && this.validarNumeros(d)==1){
-			f=true;
-		}else{
-			return f;
-		}
-		return f;
 	}
 	
-	//==========================================================
-
-	// Validacion de correo - MADE BY BERMUDEZ © ===============
-
 	public static boolean verificarCorreo(String correo) {
-		boolean v = false;
 		if ((correo.indexOf("@") != -1) && (correo.indexOf(".") != -1)) {
-			v = true;
+			return true;
 		}
-		return v;
+		return false;
 	}
 
 }
