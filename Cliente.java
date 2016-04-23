a30143766251010be3264ad22dc90101cecee41f
Anggelo Novack <behemothsl@gmail.com>
2016-04-23 13:00:04 -0500

    cambio según el cambio a persona

--- Cliente.java
+++ Cliente.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Cliente extends Persona{
 
-	public Cliente(String nDocumento,String nNombre, String nTel1, String nTel2, String nUsuario, String nContrasenia,
-			String nCorreo, String nDireccion, List<Producto> nLista) {
-		super(nDocumento,nNombre, nTel1, nTel2,  nUsuario, nContrasenia, nCorreo, nDireccion, nLista);
+	public Cliente(String nDocumento, String nNombre, String nTel1, String nTel2, String nUsuario, String nContrasenia,
+			String nCorreo, String nDireccion, List<Producto> nLista, char nTipoUsuario) {
+		super(nDocumento, nNombre, nTel1, nTel2, nUsuario, nContrasenia, nCorreo, nDireccion, nLista, nTipoUsuario);
 		// TODO Auto-generated constructor stub
 	}
 
