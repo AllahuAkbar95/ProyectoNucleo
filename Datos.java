8734f9779585f7fa53803aa9a32b0197a827de3b
esteban027 <bermu095@outlook.com>
2016-05-03 19:02:28 -0500

    Verificar Correo

--- Datos.java
+++ Datos.java
@@ -32,12 +32,26 @@ public class Datos {
 		}
 	}
 	
-	public static boolean verificarCorreo(String correo) 
-	{
-		if (correo.indexOf("@") ==1   && correo.indexOf(".") ==1 ) {
-			return true;
+	public static boolean verificarCorreo(String correo) {
+		boolean v = false;
+		int c1 = 0, c2 = 0,pos1=0,pos2=0;
+		if (correo.indexOf("@") != -1 && correo.indexOf(".") != -1) {
+
+			for (char i = 0; i < correo.length(); i++) {
+				char letra = correo.charAt(i);
+				if (letra == '@') {
+					c1++;
+					pos1=i;
+				} else if (letra == '.') {
+					c2++;
+					pos2=i;
+				}
+			}
+			if ((c1 >= 1 && c2 >= 2)&&(pos1<pos2)&&(pos1>1&&pos2>2)) {
+				v = true;
+			}
 		}
-		return false;
+		return v;
 	}
 
 
