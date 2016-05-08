d82601e447325376a9318b566ddaef7f9ceac30b
Anggelo Novack <behemothsl@gmail.com>
2016-05-08 17:27:57 -0500

    Update: ventana vinculada

--- vista/VentanaCampesino.java
+++ vista/VentanaCampesino.java
@@ -2,113 +2,107 @@ package vista;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-
+import java.awt.Dimension;
 import java.awt.Font;
-
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-
 import java.awt.Insets;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import ProyectTests.Tests.Campesino;
-import ProyectTests.Tests.Persona;
-import conexion.ConexionSQL;
 
-public class VentanaPrincipal extends JFrame implements ActionListener{
+public class VentanaCampesino extends JFrame implements ActionListener{
 	
-	private JLabel lblusuario, lblcontrasena, lblim;
-	private JTextField txtusuario;
-	private JButton btnIngresar, btnSalir, btnCrearUsuario;
-	private JPanel panelBotones ,panelImagen ,panelprin;
-	private JPasswordField passcontrasena;
-	private ImageIcon imagen;
-	private ConexionSQL conexion = new ConexionSQL();
+	private JButton btnEditarPerfil, btnVender, 
+			btnConsultarPrecio,btnCerrarSesion,btnnotificaciones;
+	private JPanel panelBotones, panelTabla, panelfinal;
+	private JTable table;
+	private Campesino usuario;
 	
 	
 	
-	//Constructor
-	public VentanaPrincipal(){
+	//CONSTRUCTOR
+	public VentanaCampesino(Campesino usuario){
+		this.usuario = usuario;
+		setTitle(" AGROLIBRE -- Sesion Inciada (Usuario)");
+		setSize(400, 320);//ancho- largo
 		
-		setTitle(" AGROLIBRE -- inicio de sesion");
-		//setSize(450, 200);//ancho- largo
 	    setResizable(false);
-		super.setSize(getPreferredSize());
-		
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);
 		
 	    //TIPOGRAFIA
 	    Font letra = new Font( "Comic Sans MS",Font.BOLD,14); 
-	    Font letra2 = new Font( "Comic Sans MS",Font.PLAIN,12);
+	   
 	    
-	    //COLORES
+	  //COLORES
 	    Color verde= new Color(20, 60, 18);
 	    
 	    
-		lblusuario= new JLabel("Usuario: ");
-		lblusuario.setFont(letra);
-		
-        txtusuario= new JTextField(10);
-        txtusuario.setFont(letra2);
-        
-        lblcontrasena= new JLabel("Contraseña: ");
-        lblcontrasena.setFont(letra);
-        
-        passcontrasena= new JPasswordField(10);
-        
-        
-        btnIngresar= new JButton("Ingresar ");
-        btnIngresar.addActionListener(this);
-        btnIngresar.setFont(letra);
-        btnIngresar.setBackground(verde);
-        btnIngresar.setForeground(Color.WHITE);
-        
-        btnSalir= new JButton("Salir ");
-        btnSalir.addActionListener(this);
-        btnSalir.setFont(letra);
-        btnSalir.setBackground(verde);
-        btnSalir.setForeground(Color.WHITE);
-       
-        
-        
-        btnCrearUsuario= new JButton("Crear Usuario  ");
-        btnCrearUsuario.addActionListener(this);
-        btnCrearUsuario.setFont(letra);
-        btnCrearUsuario.setBackground(verde);
-        btnCrearUsuario.setForeground(Color.WHITE);
-
-        
-        panelBotones = new JPanel();
-        panelBotones.setLayout(new GridBagLayout());
+	    
+	    btnEditarPerfil= new JButton("Editar Perfil ");
+	    btnEditarPerfil.addActionListener(this);
+	    btnEditarPerfil.setFont(letra);
+	    btnEditarPerfil.setBackground(verde);
+	    btnEditarPerfil.setForeground(Color.WHITE);
+	    
+	    btnConsultarPrecio= new JButton("Consultar Precio ");
+	    btnConsultarPrecio.addActionListener(this);
+	    btnConsultarPrecio.setFont(letra);
+	    btnConsultarPrecio.setBackground(verde);
+	    btnConsultarPrecio.setForeground(Color.WHITE);
+	    
+	    btnVender= new JButton("Vender ");
+	    btnVender.addActionListener(this);
+	    btnVender.setFont(letra);
+	    btnVender.setBackground(verde);
+	    btnVender.setForeground(Color.WHITE);
+	    
+	    btnnotificaciones= new JButton("Notificaciones ");
+	    btnnotificaciones.addActionListener(this);
+	    btnnotificaciones.setFont(letra);
+	    btnnotificaciones.setBackground(verde);
+	    btnnotificaciones.setForeground(Color.WHITE);
+	    
+	    btnCerrarSesion= new JButton("Cerrrar Sesion ");
+	    btnCerrarSesion.addActionListener(this);
+	    btnCerrarSesion.setFont(letra);
+	    btnCerrarSesion.setBackground(verde);
+	    btnCerrarSesion.setForeground(Color.WHITE);
+	    
+	    
+	    panelBotones= new JPanel();
+	    panelBotones.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		
-		panelImagen = new JPanel();
-		panelImagen.setLayout(new GridBagLayout());
-		
-		
-		
-		GridBagConstraints gbc2 = new GridBagConstraints();
-		
-		
-		imagen= new ImageIcon("logo2.jpg");
-		lblim= new JLabel(imagen);
-		
-		
+		panelTabla= new JPanel();
+		panelTabla.setLayout(new GridBagLayout());
+		//GridBagConstraints gbc2 = new GridBagConstraints();
 
+	        //table.setPreferredScrollableViewportSize(table.getPreferredSize());
+	        JScrollPane scrollPane = new JScrollPane(table,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	        getContentPane().add(scrollPane);
+	        
+			getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+	//=========================================================================
+		
+		
 		gbc.gridx = 0;
-		gbc.gridy = 0;
+		gbc.gridy = 2;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
@@ -116,63 +110,18 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.anchor = GridBagConstraints.WEST;
-		panelBotones.add(lblusuario, gbc);
-
-
+		panelBotones.add(btnEditarPerfil, gbc);
+	    
 		gbc.gridx = 1;
-		gbc.gridy = 0;
+		gbc.gridy = 2;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
 		gbc.weighty = 1.0;
 		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(txtusuario, gbc);
-
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		gbc.weightx = 0.0;
-		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(lblcontrasena, gbc);
-
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		gbc.weightx = 0.0;
-		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(passcontrasena, gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		gbc.weightx = 0.0;
-		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(btnIngresar, gbc);
-
-		gbc.gridx = 1;
-		gbc.gridy = 2;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		gbc.weightx = 0.0;
-		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(btnSalir, gbc);
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(btnConsultarPrecio, gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 3;
@@ -180,163 +129,138 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
 		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
+		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelBotones.add(btnCrearUsuario, gbc);
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(btnVender, gbc);
 		
-	//IMAGEN
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0.0;
+		gbc.weighty = 1.0;
+		gbc.insets = new Insets(3, 3, 3, 3);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(btnCerrarSesion, gbc);
+		
 		gbc.gridx = 0;
 		gbc.gridy = 4;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
 		gbc.weighty = 1.0;
-		gbc.insets = new Insets(7, 3, 3, 3);
+		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.fill = GridBagConstraints.BOTH;
-		gbc.anchor = GridBagConstraints.CENTER;
-		panelImagen.add(lblim, gbc2);
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(btnnotificaciones, gbc);
 		
-		panelBotones.setBackground(null);	
-
-		panelprin =new JPanel();
-		panelprin.add(panelImagen);
-		panelprin.add(panelBotones);
-		panelprin.setBackground(Color.WHITE);
+		panelfinal =new JPanel();
+		
+		//panelfinal.add(panelTabla);
+		panelfinal.add(panelBotones);
+		
+		//panelTabla.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
+		//panelTabla.setBackground(Color.WHITE);
 		panelBotones.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
-		panelImagen.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
+		panelBotones.setBackground(Color.WHITE);
+		panelfinal.setBackground(Color.WHITE);
 		
-		this.add(panelprin);
+		this.add(panelfinal);
 		
-
-
+		
 	}
 	
 
 	
 	public void actionPerformed(ActionEvent evento) {
-		
-if(evento.getSource()==btnIngresar){
-			
-			Persona usuario = conexion.iniciarUsuario(txtusuario.getText(), passcontrasena.getText());
-			
-			if(usuario != null)
-			{
-				if(usuario.getTipoUsurio().charAt(0) == 'C')
-				{
-					Campesino camp = new Campesino(usuario.getDocumento(), usuario.getNombre(), usuario.getTel1(), usuario.getTel2(), usuario.getUsuario(), usuario.getContrasenia(), usuario.getCorreo(), usuario.getDireccion(), usuario.getListaProductos(), usuario.getTipoUsurio().charAt(0));
-				//para mirar la ventana de campesino
-				VentanaCampesino vc=new VentanaCampesino(camp);
-				vc.setVisible(true);
-				vc.pack(); //en caso de que se haga grande
-				vc.setLocationRelativeTo(null);
-				this.setVisible(false);
-				}
-				else if(usuario.getTipoUsurio().charAt(0) == 'U')
-				{
-					//para mirar la ventana de cliente
-					VentanaCliente vcl=new VentanaCliente();
-					vcl.setVisible(true);
-					vcl.setLocationRelativeTo(null);
-					this.setVisible(false);
-				}else{
-					JOptionPane.showMessageDialog(this, "Usuario y/o contraseña incorrectos");
-				}
-				
-				
-			}else{
-				JOptionPane.showMessageDialog(this, "Usuario y/o contraseña incorrectos");
-			}
-			
-			
-			
-		
-		}
-		
-		if(evento.getSource()==btnCrearUsuario){
-			
-			VentanaNuevoUsuario vnu=new VentanaNuevoUsuario();
-			vnu.setVisible(true);
-			vnu.pack();
-			vnu.setLocationRelativeTo(null);
+
+		if(evento.getSource()==btnEditarPerfil){
+			VentanaEditarUsuario vep=new VentanaEditarUsuario(this.usuario);
+			vep.setVisible(true);
+			vep.pack();
+			vep.setLocationRelativeTo(null);
 			this.setVisible(false);
-
+		
+			
+			
 		}
 		
-		if(evento.getSource()==btnSalir){
-			System.exit(0);
+		if(evento.getSource()==btnConsultarPrecio){
+			VentanaPrecio vp=new VentanaPrecio(this.usuario);
+			vp.setVisible(true);
+			vp.setLocationRelativeTo(null);
+			this.setVisible(false);
+		}
+			
+		if(evento.getSource()==btnCerrarSesion){
+			JOptionPane.showMessageDialog(this, "Sesion cerrada conrectamente! ");
+			VentanaPrincipal vp=new VentanaPrincipal();
+			vp.setVisible(true);
+			vp.pack();
+			vp.setLocationRelativeTo(null);
+			this.setVisible(false);
 		}
 		
+		if(evento.getSource()==btnVender){
+			VentanaVenderProducto vvp=new VentanaVenderProducto(this.usuario);
+			vvp.setVisible(true);
+			vvp.pack();
+			vvp.setLocationRelativeTo(null);
+			this.setVisible(false);
+			
+		}
 	}
 
 
-	public static void main(String[] args) {
-		
-		VentanaPrincipal vp= new VentanaPrincipal();
-		vp.pack();
-		vp.setVisible(true);
-		vp.setLocationRelativeTo(null);
-
-	}
-
-//METODOS GETTERS AND SETTERS
-	public JLabel getLblusuario() {
-		return lblusuario;
+	public JButton getBtnEditarPerfil() {
+		return btnEditarPerfil;
 	}
 
 
-	public void setLblusuario(JLabel lblusuario) {
-		this.lblusuario = lblusuario;
+	public void setBtnEditarPerfil(JButton btnEditarPerfil) {
+		this.btnEditarPerfil = btnEditarPerfil;
 	}
 
 
-	public JLabel getLblcontrasena() {
-		return lblcontrasena;
+	public JButton getBtnVender() {
+		return btnVender;
 	}
 
 
-	public void setLblcontrasena(JLabel lblcontrasena) {
-		this.lblcontrasena = lblcontrasena;
+	public void setBtnVender(JButton btnVender) {
+		this.btnVender = btnVender;
 	}
 
 
-	public JTextField getTxtusuario() {
-		return txtusuario;
+	public JButton getBtnConsultarPrecio() {
+		return btnConsultarPrecio;
 	}
 
 
-	public void setTxtusuario(JTextField txtusuario) {
-		this.txtusuario = txtusuario;
+	public void setBtnConsultarPrecio(JButton btnConsultarPrecio) {
+		this.btnConsultarPrecio = btnConsultarPrecio;
 	}
 
 
-	public JButton getBtnIngresar() {
-		return btnIngresar;
+	public JButton getBtnCerrarSesion() {
+		return btnCerrarSesion;
 	}
 
 
-	public void setBtnIngresar(JButton btnIngresar) {
-		this.btnIngresar = btnIngresar;
+	public void setBtnCerrarSesion(JButton btnCerrarSesion) {
+		this.btnCerrarSesion = btnCerrarSesion;
 	}
 
 
-	public JButton getBtnSalir() {
-		return btnSalir;
+	public JButton getBntnotificaciones() {
+		return btnnotificaciones;
 	}
 
 
-	public void setBtnSalir(JButton btnSalir) {
-		this.btnSalir = btnSalir;
-	}
-
-
-	public JButton getBtnCrearUsuario() {
-		return btnCrearUsuario;
-	}
-
-
-	public void setBtnCrearUsuario(JButton btnCrearUsuario) {
-		this.btnCrearUsuario = btnCrearUsuario;
+	public void setBntnotificaciones(JButton bntnotificaciones) {
+		this.btnnotificaciones = bntnotificaciones;
 	}
 
 
@@ -350,17 +274,22 @@ if(evento.getSource()==btnIngresar){
 	}
 
 
-	public JPasswordField getPasscontrasena() {
-		return passcontrasena;
+	public JPanel getPanelTabla() {
+		return panelTabla;
 	}
 
 
-	public void setPasscontrasena(JPasswordField passcontrasena) {
-		this.passcontrasena = passcontrasena;
+	public void setPanelTabla(JPanel panelTabla) {
+		this.panelTabla = panelTabla;
 	}
-	
-	
-	
 
 
+	public JPanel getPanel() {
+		return panelfinal;
+	}
+
+
+	public void setPanel(JPanel panel) {
+		this.panelfinal = panel;
+	}
 }
