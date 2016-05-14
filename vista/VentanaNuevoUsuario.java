a24ea52dc18ccca08d91d3b3276bf1bc6b589bd3
Anggelo Novack <behemothsl@gmail.com>
2016-05-14 11:41:39 -0500

    Update VentanaNuevoUsuario.java

--- vista/VentanaNuevoUsuario.java
+++ vista/VentanaNuevoUsuario.java
@@ -1,4 +1,4 @@
-package vista_26_04_16;
+package vista;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -7,6 +7,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -20,16 +22,20 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import ProyectTests.Tests.Datos;
+import ProyectTests.Tests.Campesino;
+import ProyectTests.Tests.Cliente;
+import ProyectTests.Tests.Producto;
+import ProyectTests.Tests.Validar;
+import conexion.ConexionSQL;
 
 
 public class VentanaNuevoUsuario extends JFrame implements ActionListener {
-	
-	//private JCheckBox checkTipoCliente,checkTipoCampesino;
+	private ConexionSQL conexion = new ConexionSQL();
+	private JCheckBox checkTipoCliente,checkTipoCampesino;
 	private JLabel lblnombre, lbltelefono1,lbltelefono2, lbldocumento, lblusuario,
-	lblcontrasena,lblim, lblcorreo;
+	lblcontrasena,lblim, lblcorreo, lbldireccion;
 	private JTextField txtnombre,txttelefono1,txttelefono2,txtdocumento,txtusuario
-	,txtcorreo;
+	,txtcorreo,txtdireccion;
 	private JPasswordField contrasena;
 	private JPanel panelBotones, panel,panelcheck;
 	private JButton btnCrearUsuario, btnAtras;
@@ -53,22 +59,22 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 	    //COLORES
 	    Color verde= new Color(20, 60, 18);
 		
-//	    checkTipoCliente=new JCheckBox("Cliente");
-//	    checkTipoCliente.addActionListener(this);
-//	    checkTipoCliente.setFont(letra);
-//	    checkTipoCliente.setSelected(false);
-//	    checkTipoCliente.setBackground(Color.WHITE);
-//	 
-//	    
-//	    checkTipoCampesino=new JCheckBox("Campesino");
-//	    checkTipoCampesino.addActionListener(this);
-//	    checkTipoCampesino.setFont(letra);
-//	    checkTipoCampesino.setSelected(false);
-//	    checkTipoCampesino.setBackground(Color.WHITE);
-//		
-//		grupoCheck= new ButtonGroup();
-//		grupoCheck.add(checkTipoCampesino);
-//		grupoCheck.add(checkTipoCliente);
+	    checkTipoCliente=new JCheckBox("Cliente");
+	    checkTipoCliente.addActionListener(this);
+	    checkTipoCliente.setFont(letra);
+	    checkTipoCliente.setSelected(false);
+	    checkTipoCliente.setBackground(Color.WHITE);
+	 
+	    
+	    checkTipoCampesino=new JCheckBox("Campesino");
+	    checkTipoCampesino.addActionListener(this);
+	    checkTipoCampesino.setFont(letra);
+	    checkTipoCampesino.setSelected(false);
+	    checkTipoCampesino.setBackground(Color.WHITE);
+		
+		grupoCheck= new ButtonGroup();
+		grupoCheck.add(checkTipoCampesino);
+		grupoCheck.add(checkTipoCliente);
 		
 		
 		
@@ -99,16 +105,21 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 		txttelefono1.setFont(letra2);
 		
 		
-		lbltelefono2=new JLabel("Telefono 2: ");
+		lbltelefono2=new JLabel("Telefono 2 (opcional): ");
 		lbltelefono2.setFont(letra);
 		txttelefono2=new JTextField(10);
 		txttelefono2.setFont(letra2);
 		
-		lblcorreo=new JLabel("Correo: ");
+		lblcorreo=new JLabel("Correo (opcional): ");
 		lblcorreo.setFont(letra);
 		txtcorreo=new JTextField(10);
 		txtcorreo.setFont(letra2);
-	
+		
+		lbldireccion=new JLabel("Direccion (opcional): ");
+		lbldireccion.setFont(letra);
+		txtdireccion=new JTextField(10);
+		txtdireccion.setFont(letra2);
+		
 		btnCrearUsuario=new JButton("CrearUsuario");
 		btnCrearUsuario.addActionListener(this);
 		btnCrearUsuario.setFont(letra);
@@ -133,28 +144,28 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 		GridBagConstraints gbc=new GridBagConstraints();
 		
 		
-//		gbc2.gridx = 1;
-//		gbc2.gridy = 0;
-//		gbc2.gridwidth = 1;
-//		gbc2.gridheight = 1;
-//		gbc2.weightx = 0.0;
-//		gbc2.weighty = 1.0;
-//		gbc2.insets = new Insets(3, 3, 3, 3);
-//		gbc2.fill = GridBagConstraints.BOTH;
-//		gbc2.anchor = GridBagConstraints.WEST;
-//		panelcheck.add(checkTipoCliente, gbc2);
-//		
+		gbc2.gridx = 1;
+		gbc2.gridy = 0;
+		gbc2.gridwidth = 1;
+		gbc2.gridheight = 1;
+		gbc2.weightx = 0.0;
+		gbc2.weighty = 1.0;
+		gbc2.insets = new Insets(3, 3, 3, 3);
+		gbc2.fill = GridBagConstraints.BOTH;
+		gbc2.anchor = GridBagConstraints.WEST;
+		panelcheck.add(checkTipoCliente, gbc2);
 		
-//		gbc2.gridx = 2;
-//		gbc2.gridy = 0;
-//		gbc2.gridwidth = 1;
-//		gbc2.gridheight = 1;
-//		gbc2.weightx = 0.0;
-//		gbc2.weighty = 1.0;
-//		gbc2.insets = new Insets(3, 3, 3, 3);
-//		gbc2.fill = GridBagConstraints.BOTH;
-//		gbc2.anchor = GridBagConstraints.WEST;
-//		panelcheck.add(checkTipoCampesino, gbc2);
+		
+		gbc2.gridx = 2;
+		gbc2.gridy = 0;
+		gbc2.gridwidth = 1;
+		gbc2.gridheight = 1;
+		gbc2.weightx = 0.0;
+		gbc2.weighty = 1.0;
+		gbc2.insets = new Insets(3, 3, 3, 3);
+		gbc2.fill = GridBagConstraints.BOTH;
+		gbc2.anchor = GridBagConstraints.WEST;
+		panelcheck.add(checkTipoCampesino, gbc2);
 		
 		gbc2.gridx = 0;
 		gbc2.gridy = 0;
@@ -323,7 +334,30 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 		panelBotones.add(txtcorreo, gbc);
 		
 		gbc.gridx = 0;
-		gbc.gridy = 9;
+		gbc.gridy = 8;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0.0;
+		gbc.weighty = 1.0;
+		gbc.insets = new Insets(3, 3, 3, 3);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(lbldireccion, gbc);
+		
+		
+		gbc.gridx = 1;
+		gbc.gridy = 8;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0.0;
+		gbc.weighty = 1.0;
+		gbc.insets = new Insets(3, 3, 3, 3);
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.anchor = GridBagConstraints.WEST;
+		panelBotones.add(txtdireccion, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 10;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
@@ -334,7 +368,7 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 		panelBotones.add(btnCrearUsuario, gbc);
 		
 		gbc.gridx = 1;
-		gbc.gridy = 9;
+		gbc.gridy = 10;
 		gbc.gridwidth = 1;
 		gbc.gridheight = 1;
 		gbc.weightx = 0.0;
@@ -352,10 +386,10 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 		panelcheck.setBackground(Color.WHITE);
 		panel.setBackground(Color.WHITE);
 		
-		//panelBotones.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
+		panelBotones.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
 		panelBotones.setBorder(BorderFactory.createTitledBorder("Llene la informacion"));
-		//panelcheck.setBorder(BorderFactory.createTitledBorder("Seleccione Tipo de Usuario"));
-		//panelcheck.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
+		panelcheck.setBorder(BorderFactory.createTitledBorder("Seleccione Tipo de Usuario"));
+		panelcheck.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, verde));
 		
 		
 		this.add(panel);
@@ -366,45 +400,71 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 	
 	public void actionPerformed(ActionEvent evento) {
 		if(evento.getSource()==btnCrearUsuario){
-			Datos d=new Datos();
 			
 			if(txtnombre.getText().length()==0 || txtdocumento.getText().length()==0 || txtusuario.getText().length()==0
-					|| contrasena.getText().length()==0 || txttelefono1.getText().length()==0 || txttelefono2.getText().length()==0
-					|| txtcorreo.getText().length()==0){
-				JOptionPane.showMessageDialog(this, "Debe llenar TODOS los campos por favor. ");
+					|| contrasena.getText().length()==0 || txttelefono1.getText().length()==0){
+				JOptionPane.showMessageDialog(this, "Debe llenar los campos obligatorios por favor. ");
 				
 			}
-			else if(d.validarLetras(txtnombre.getText())==false){
+			else if(!Validar.letras(txtnombre.getText())){
 				JOptionPane.showMessageDialog(this, "NO se admiten numeros en el nombre ");
 			}
-			else if(d.validarNumeros(txtdocumento.getText())==false ){
-				JOptionPane.showMessageDialog(this, "NO se admiten letras en el Documento ");
+			else if(!Validar.tamanio(8, contrasena.getText(), 15)){
+				JOptionPane.showMessageDialog(this, "La contraseña debe ser entre 8 y 15 caracteres");
 			}
-			else if(d.validarNumeros(txttelefono1.getText())==false){
+			else if(Validar.tamanio(4, txtnombre.getText(), 20)==false){
+				JOptionPane.showMessageDialog(this, "El nombre debe ser minimo de 4 caracteres.");
+			}
+			else if(!Validar.numeros(txtdocumento.getText()) || !Validar.tamanio(10, txtdocumento.getText(), 10) ){
+				JOptionPane.showMessageDialog(this, "NO se admiten letras en el Documento y debe tener 10 caracteres");
+			}
+			else if(!Validar.numeros(txttelefono1.getText())){
 				JOptionPane.showMessageDialog(this, "NO se admiten letras en el Telefono 1");
 			}
-			else if(d.validarNumeros(txttelefono2.getText())==false){
-				JOptionPane.showMessageDialog(this,  "NO se admiten letras en el Telefono 2");
-
+			else if ((Validar.tamanio(7, txttelefono1.getText(), 10)==false)){
+				JOptionPane.showMessageDialog(this, "El telefono  1 debe ser de 7 y 10 digitos");
 			}
-//			else if(d.verificarCorreo(txtcorreo.getText())==false){
-//				JOptionPane.showMessageDialog(this, "El Correo no es valido");
-//			}
+			else if(txttelefono2.getText().length()!=0){
+				if(Validar.numeros(txttelefono2.getText())==false){
+					JOptionPane.showMessageDialog(this, "NO se admiten letras en el Telefono 2");
+				}
+				else if ((Validar.tamanio(7, txttelefono1.getText(), 10)==false)){
+					JOptionPane.showMessageDialog(this, "El telefono  2 debe ser de 7 y 10 digitos");
+				}
+				
+			}
+			else if(txtcorreo.getText().length() !=0 ){
+				if(Validar.correo(txtcorreo.getText()) || Validar.tamanio(0, txtcorreo.getText(), 20))
+				JOptionPane.showMessageDialog(this, "El Correo no es valido");
+			}
 			else if(txttelefono1.getText().equals(txttelefono2.getText())){
 				JOptionPane.showMessageDialog(this, "NO  puede usar el mismo numero telefonico en ambos campos");
 			}
+			else
+			{
+				if(checkTipoCampesino.isSelected())
+				{
+					List<Producto> lista = new LinkedList<Producto>();
+					Campesino usuario = new Campesino(txtdocumento.getText(), txtnombre.getText(), txttelefono1.getText(), txttelefono2.getText(), txtusuario.getText(),contrasena.getText(),txtcorreo.getText(), txtdireccion.getText(), lista, 'C');
+					if(conexion.insertarUsuario(usuario))
+						JOptionPane.showMessageDialog(this, "Usuario agregado correctamente");
+					else
+						JOptionPane.showMessageDialog(this, "Se produjo un error, usuario ya existente");
+					
+				}else if(checkTipoCliente.isSelected())
+				{
+					List<Producto> lista = new LinkedList<Producto>();
+					Cliente usuario = new Cliente(txtdocumento.getText(), txtnombre.getText(), txttelefono1.getText(), txttelefono2.getText(), txtusuario.getText(),contrasena.getText(),txtcorreo.getText(), txtdireccion.getText(), lista, 'U');
+					if(conexion.insertarUsuario(usuario))
+						JOptionPane.showMessageDialog(this, "Usuario agregado correctamente");
+					else
+						JOptionPane.showMessageDialog(this, "Se produjo un error, usuario o documento ya existente");
+				}else{
+					JOptionPane.showMessageDialog(this, "Seleccione un tipo de usuario");
+				}
+			}
 		}
-		
-//		if(evento.getSource()==checkTipoCliente){
-//			//btnCrearUsuario.setEnabled(false);
-//			txtcorreo.setEnabled(true);
-//		}
-//		else if(evento.getSource()==checkTipoCampesino){
-//			//btnCrearUsuario.setEnabled(true);
-//			txtcorreo.setEnabled(false);
-//			
-//		}
-//		
+				
 		if( evento.getSource()==btnAtras){
 			VentanaPrincipal vp=new VentanaPrincipal();
 			vp.setVisible(true);
@@ -414,24 +474,6 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 				
 		}
 	}
-
-	
-	//GETTERS AND SETTERS
-//	public JCheckBox getCheckTipoCliente() {
-//		return checkTipoCliente;
-//	}
-//
-//	public void setCheckTipoCliente(JCheckBox checkTipoCliente) {
-//		this.checkTipoCliente = checkTipoCliente;
-//	}
-//
-//	public JCheckBox getCheckTipoCampesino() {
-//		return checkTipoCampesino;
-//	}
-//
-//	public void setCheckTipoCampesino(JCheckBox checkTipoCampesino) {
-//		this.checkTipoCampesino = checkTipoCampesino;
-//	}
 
 	public JLabel getLblnombre() {
 		return lblnombre;
@@ -608,8 +650,24 @@ public class VentanaNuevoUsuario extends JFrame implements ActionListener {
 	public void setTxtcorreo(JTextField txtcorreo) {
 		this.txtcorreo = txtcorreo;
 	}
-	
-	
-	
 
+
+	public JLabel getLbldireccion() {
+		return lbldireccion;
+	}
+
+
+	public void setLbldireccion(JLabel lbldireccion) {
+		this.lbldireccion = lbldireccion;
+	}
+
+
+	public JTextField getTxtdireccion() {
+		return txtdireccion;
+	}
+
+
+	public void setTxtdireccion(JTextField txtdireccion) {
+		this.txtdireccion = txtdireccion;
+	}
 }
